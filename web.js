48de69de099cc541b2c873bee360113d143c7ac2
Neena Kapur <neena.kapur@tufts.edu>
2013-04-14 17:07:56 -0400

    working on app.post, proper headers included

--- web.js
+++ web.js
@@ -13,18 +13,39 @@ var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
 	db = databaseConnection;
 });
 
+app.post('/submit.json', function(request, response){
+	response.header['Access-Control-Allow-Origin', '*'];
+	request.header['Access-Control-Allow-Headers', 'X-Requested-With'];
+	
+	var username=request.body.username;
+	var score=request.body.score;
+	var game_title=request.body.game_title;
+	var created_at=request.Date;
+	
+	db.collection("highscores", function(error, collection){
+		db.collection.insert({'game_title'=game_title, 'username': username, 'score'=score, 'created_at': created_at});
+	});
+	response.set('Content-Type', 'text/html');
+	response.send();
+});
+
 app.get('/', function (request, response) {
 	/*
 		db.collection('NAME_OF_YOUR_COLLECTON_HERE...', function(er, collection) {
 			collection.find()...
 	*/
 	response.set('Content-Type', 'text/html');
-	response.send('<p>Hi!</p>');
+	response.send('game_title</br>username</br>score</br><created_at');
 });
 
-app.get('/data.json', function(request, response) {
+app.get('/highscores.json', function(request, response) {
 	response.set('Content-Type', 'text/json');
 	response.send('{"status":"good"}');
+});
+
+app.get('/usersearch', function(request, response){
+
+
 });
 
 app.get('/fool', function(request, response) {
