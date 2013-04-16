6c881bd9f15b0379ce2dc955794014df50dcf5b6
Neena Kapur <neena.kapur@tufts.edu>
2013-04-15 21:09:51 -0400

    a[] instead of a.

--- web.js
+++ web.js
@@ -38,11 +38,11 @@ app.get('/highscores.json', function(request, response) {
 	db.collection('highscores', function(err, collection){
 		collection.find({'game_title':game_title}).toArray(function (err, documents){
 			documents.sort(function(a,b) {
-				 if (a['game_title'] == b.['game_title']) {
-				 	return(b.['score']-a.['score']);
+				 if (a['game_title'] == b['game_title']) {
+				 	return(b['score']-a['score']);
 				 }
 				 else{
-				 	return (a.['game_title'].localeCompare(b.['game_title']));
+				 	return (a['game_title'].localeCompare(b['game_title']));
 				}
 			});
 		});
