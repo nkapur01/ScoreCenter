44e9de170490511075087792bf81299a93dd8cf7
Neena Kapur <neena.kapur@tufts.edu>
2013-04-18 12:45:41 -0400

    usersearch .get making submit form

--- web.js
+++ web.js
@@ -46,9 +46,9 @@ app.get('/highscores.json', function(request, response) {
 });
 
 app.get('/', function (request, response) {
-
 	response.header('Access-Control-Allow-Origin','*');
 	request.header('Access-Control-Allow-Headers', 'X-Requested-With');
+	
 	db.collection('highscores', function(err, collection){
 		collection.find().toArray(function(err, documents){
 			response.set('Content-Type', 'text/json');
@@ -58,21 +58,16 @@ app.get('/', function (request, response) {
 });
 
 
-app.get('/usersearch', function(request, response){
-	response.header('Access-Control-Allow-Origin','*');
-	request.header('Access-Control-Allow-Headers', 'X-Requested-With');
-
-	var username=prompt("Please enter the username you'd like to find:");
-
-	db.collection('highscores', function(err, collection){
-		collection.find({'username':username}).toArray(function(err, documents){
-			response.set('Content-Type', 'text/json');
-			response.send(documents);
-		});
-	});
-
-});
-
+ app.get('/usersearch', function(request, response){
+ 	response.header('Access-Control-Allow-Origin','*');
+ 	request.header('Access-Control-Allow-Headers', 'X-Requested-With');
+ 	console.log("loosah");
+ 	
+ 	
+ 	request.set('Content-Type', 'text/html');
+ 	request.send('<!DOCTYPE html><html><head><title>User Search</title></head><body><form name="search" action="results" method="post">Username: <input type="text" name="username"><input type="submit" value="Submit"></form></body></html>')
+ 	
+ });
 
 app.get('/fool', function(request, response) {
 	response.set('Content-Type', 'text/html');
